685b4f4b1b604e9786b52b6d0bacfbc1d3abeff0
Prajeet Kumar Patibandla <peppyprajeet@gmail.com>
2017-01-27 20:15:27 -0800

    Final

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -1,153 +1,174 @@
 'use strict';
+
 angular.module('confusionApp')
- 
 
-    .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
- 
-        $scope.tab = 1;
-        $scope.filtText = '';
-        $scope.showDetails = false;
-  
-        $scope.showMenu = false;
-        $scope.message = "Loading ...";
-        $scope.dishes= {};
-            menuFactory.getDishes()
-            .then(
-                function(response) {
-                    $scope.dishes = response.data;
-                    $scope.showMenu = true;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                }
-            );
+.controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
- 
-        $scope.select = function(setTab) {
-            $scope.tab = setTab;
-    
-    
-            if (setTab === 2) {
-               $scope.filtText = "appetizer";
-            } 
-            else if (setTab === 3) {
-               $scope.filtText = "mains";
-            }   
-            else if (setTab === 4) {
-              $scope.filtText = "dessert";   
-            }   
-            else {     
-              $scope.filtText = "";    
-            } 
-        };
-        
-        $scope.isSelected = function (checkTab) {
-            return ($scope.tab === checkTab);
-        };
-  
-        $scope.toggleDetails = function() {
-            $scope.showDetails = !$scope.showDetails;
-        };
-    }])
+  $scope.tab = 1;
+  $scope.filtText = '';
+  $scope.showDetails = false;
+  $scope.showMenu = false;
+  $scope.message = "Loading ...";
+  $scope.dishes = {};
+  menuFactory.getDishes().query(
+    function(response){
+      $scope.dishes = response;
+      $scope.showMenu = true;
+    },
+    function(response){
+      $scope.message = "Error: "+response.status + " " + response.statusText;
+    }
+  );
 
 
-    .controller('ContactController', ['$scope', function($scope) {
-        
-        $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-        
-        var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-        
-        $scope.channels = channels;
-        $scope.invalidChannelSelection = false;
-    }])
+  $scope.select = function(setTab) {
+    $scope.tab = setTab;
 
+    if (setTab === 2) {
+      $scope.filtText = "appetizer";
+    }
+    else if (setTab === 3) {
+      $scope.filtText = "mains";
+    }
+    else if (setTab === 4) {
+      $scope.filtText = "dessert";
+    }
+    else {
+      $scope.filtText = "";
+    }
+  };
 
-    .controller('FeedbackController', ['$scope', function($scope) {
- 
-        $scope.sendFeedback = function() {
-            console.log($scope.feedback);
-            
-            if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
-                $scope.invalidChannelSelection = true;
-                console.log('incorrect');
-            }               
-            else {
-                $scope.invalidChannelSelection = false;   
-                $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-                $scope.feedback.mychannel="";
-                $scope.feedbackForm.$setPristine();
-                console.log($scope.feedback);
-            }
-        };
-    }])
+  $scope.isSelected = function (checkTab) {
+    return ($scope.tab === checkTab);
+  };
 
+  $scope.toggleDetails = function() {
+    $scope.showDetails = !$scope.showDetails;
+  };
+}])
 
-    .controller('DishDetailController', ['$scope', '$stateParams','menuFactory', function($scope, $stateParams, menuFactory) {
- 
-        $scope.dish = {};
-            $scope.showDish = false;
-            $scope.message="Loading ...";
-                        menuFactory.getDish(parseInt($stateParams.id,10))
-            .then(
-                function(response){
-                    $scope.dish = response.data;
-                    $scope.showDish=true;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                }
-            );
-    }])
+.controller('ContactController', ['$scope', function($scope) {
 
+  $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
 
-    .controller('DishCommentController', ['$scope', function($scope) {
-        
-        $scope.comment = {author: "", rating: 5, comment: "", date: new Date().toISOString()};
-        
-        //Step 1: Create a JavaScript object to hold the comment from the form
-        $scope.submitComment = function () {
-            console.log($scope.comment);
-            
-            //Step 2: This is how you record the date
-            $scope.comment.date = new Date().toISOString();
-            
-            // Step 3: Push your comment into the dish's comment array
-            $scope.dish.comments.push($scope.comment);
-            
-            //Step 4: reset your form to pristine
-            $scope.commentForm.$setPristine();
-            
-            //Step 5: reset your JavaScript object that holds your comment
-            $scope.comment = {author: "", rating: 5, comment: "", date: new Date().toISOString()};
-            console.log($scope.comment);
-        };
-    }])
+  var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
 
-    .controller('IndexController', [ '$scope', 'menuFactory', 'corporateFactory', function ($scope, menuFactory, corporateFactory) {
+  $scope.channels = channels;
+  $scope.invalidChannelSelection = false;
 
-       $scope.dish = {};
-                        $scope.showDish = false;
-                        $scope.message="Loading ...";
+}])
 
-                        menuFactory.getDish(0)
-                        .then(
-                            function(response){
-                                $scope.dish = response.data;
-                                $scope.showDish = true;
-                            },
-                            function(response) {
-                                $scope.message = "Error: "+response.status + " " + response.statusText;
-                            }
-                        ); 
-      $scope.promotion = menuFactory.getPromotion(0);
-      $scope.leader = corporateFactory.getLeader(3);   
-    
-    }])
+.controller('FeedbackController', ['$scope', 'feedbackFactory', function($scope, feedbackFactory) {
 
+  $scope.sendFeedback = function() {
 
-    .controller('AboutController', [ '$scope', 'corporateFactory', function ($scope, corporateFactory) {
+    console.log($scope.feedback);
 
-      $scope.leaders = corporateFactory.getLeaders();   
-    
-    }])
+    if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
+      $scope.invalidChannelSelection = true;
+      console.log('incorrect');
+    }
+    else {
+
+      feedbackFactory.sendFeedback($scope.feedback);
+      $scope.invalidChannelSelection = false;
+      $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+      $scope.feedback.mychannel="";
+      $scope.feedbackForm.$setPristine();
+      console.log($scope.feedback);
+    }
+  };
+}])
+
+.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+
+  $scope.showDish = false;
+  $scope.message="Loading ...";
+  $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+    .$promise.then(
+      function(response){
+        $scope.dish = response;
+        $scope.showDish = true;
+      },
+      function(response) {
+        $scope.message = "Error: "+response.status + " " + response.statusText;
+      }
+    );
+
+}])
+
+.controller('DishCommentController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+
+  $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+
+  $scope.submitComment = function () {
+    $scope.mycomment.date = new Date().toISOString();
+    console.log($scope.mycomment);
+    $scope.dish.comments.push($scope.mycomment);
+    menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
+    $scope.commentForm.$setPristine();
+    $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+  };
+}])
+
+// implement the IndexController and About Controller here
+
+.controller('IndexController',['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory){
+  $scope.monthPromotions = [];
+  $scope.messageMonthProm="Loading ...";
+  $scope.showMonthProm=false;
+  menuFactory.getPromotions().query(
+    function(response){
+      $scope.monthPromotions=response;
+      $scope.showMonthProm=true;
+    },
+    function(response){
+      $scope.messageMonthProm = "Error: "+response.status + " " + response.statusText;
+    }
+  );
+
+  $scope.showDish = false;
+  $scope.message="Loading ...";
+  $scope.recommendedDish = {};
+  menuFactory.getDishes().get({id:0})
+    .$promise.then(
+      function(response){
+        $scope.recommendedDish = response;
+        $scope.showDish = true;
+      },
+      function(response) {
+        $scope.message = "Error: "+response.status + " " + response.statusText;
+      }
+    );
+
+    $scope.showExChef = false;
+    $scope.messageExChef="Loading ...";
+    $scope.executiveChef = {};
+    corporateFactory.getLeaders().get({id:3})
+      .$promise.then(
+        function(response){
+          $scope.executiveChef = response;
+          $scope.showExChef = true;
+        },
+        function(response) {
+          $scope.messageExChef = "Error: "+response.status + " " + response.statusText;
+        }
+      );
+
+}])
+
+.controller('AboutController',['$scope', 'corporateFactory', function($scope, corporateFactory){
+  $scope.message = "Loading...";
+  $scope.showLeadership = false;
+  $scope.corporateLeadership = [];
+  corporateFactory.getLeaders().query(
+    function(response){
+      $scope.corporateLeadership = response;
+      $scope.showLeadership = true;
+    },
+    function(response) {
+      $scope.message = "Error: "+response.status + " " + response.statusText;
+    }
+  );
+}])
 ;
--- conFusion/gulpfile.js
+++ conFusion/gulpfile.js
@@ -1,19 +1,20 @@
+'use strict'
 var gulp = require('gulp'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish'),
-    uglify = require('gulp-uglify'),
-    usemin = require('gulp-usemin'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    changed = require('gulp-changed'),
-    rev = require('gulp-rev'),
-    browserSync = require('browser-sync'),
-    ngannotate = require('gulp-ng-annotate'),
-    del = require('del');
+minifycss = require('gulp-minify-css'),
+jshint = require('gulp-jshint'),
+stylish = require('jshint-stylish'),
+uglify = require('gulp-uglify'),
+usemin = require('gulp-usemin'),
+imagemin = require('gulp-imagemin'),
+rename = require('gulp-rename'),
+concat = require('gulp-concat'),
+notify = require('gulp-notify'),
+cache = require('gulp-cache'),
+changed = require('gulp-changed'),
+rev = require('gulp-rev'),
+browserSync = require('browser-sync'),
+del = require('del'),
+ngannotate = require('gulp-ng-annotate');
 
 gulp.task('jshint', function() {
   return gulp.src('app/scripts/**/*.js')
@@ -21,64 +22,72 @@ gulp.task('jshint', function() {
   .pipe(jshint.reporter(stylish));
 });
 
+// Clean
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+  gulp.start('usemin', 'imagemin','copyfonts', 'move');
+});
+
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/**/*.html')
-      .pipe(usemin({
-        css:[minifycss(),rev()],
-        js: [ngannotate(),uglify(),rev()]
-      }))
-      .pipe(gulp.dest('dist/'));
+  return gulp.src('./app/index.html')
+  .pipe(usemin({
+    css:[minifycss(),rev()],
+    js: [ngannotate(),uglify(),rev()]
+  }))
+  .pipe(gulp.dest('dist/'));
 });
 
 // Images
 gulp.task('imagemin', function() {
   return del(['dist/images']), gulp.src('app/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/images'))
-    .pipe(notify({ message: 'Images task complete' }));
-});
-
-// Clean
-gulp.task('clean', function() {
-    return del(['dist']);
+  .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+  .pipe(gulp.dest('dist/images'))
+  .pipe(notify({ message: 'Images task complete' }));
 });
 
 gulp.task('copyfonts', ['clean'], function() {
-   gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
-   .pipe(gulp.dest('./dist/fonts'));
-   gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
-   .pipe(gulp.dest('./dist/fonts'));
+  gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+  .pipe(gulp.dest('./dist/fonts'));
+  gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
+  .pipe(gulp.dest('./dist/fonts'));
 });
 
-// Default task
-gulp.task('default', ['clean'], function() {
-    gulp.start('usemin', 'imagemin','copyfonts');
+//Move other html files
+gulp.task('move', function(){
+  gulp.src(['./app/**/*.html','!./app/index.html'])
+  .pipe(gulp.dest('dist'));
 });
-
 // Watch
 gulp.task('watch', ['browser-sync'], function() {
   // Watch .js files
-  gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['usemin']);
-      // Watch image files
+  gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/index.html}', ['usemin']);
+  // Watch image files
   gulp.watch('app/images/**/*', ['imagemin']);
+
+  gulp.watch('{app/**/*.html,!app/index.html}', ['move']);
+
 
 });
 
 gulp.task('browser-sync', ['default'], function () {
-   var files = [
-      'app/**/*.html',
-      'app/styles/**/*.css',
-      'app/images/**/*.png',
-      'app/scripts/**/*.js',
-      'dist/**/*'
-   ];
+  var files = [
+    'app/**/*.html',
+    'app/styles/**/*.css',
+    'app/images/**/*.png',
+    'app/scripts/**/*.js',
+    'dist/**/*'
+  ];
 
-   browserSync.init(files, {
-      server: {
-         baseDir: "dist",
-         index: "index.html"
-      }
-   });
-        // Watch any files in dist/, reload on change
+  browserSync.init(files, {
+    server: {
+      baseDir: "dist"
+    },
+    port: 3010
+  });
+  // Watch any files in dist/, reload on change
   gulp.watch(['dist/**']).on('change', browserSync.reload);
-    });
+});
